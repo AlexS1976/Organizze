0656cb60936ad1e85f108a2ad30a466517dacfd3
Alex Silva <damasioesilva@gmail.com>
2021-02-23 20:58:14 -0300

    add login e tratamentos

--- app/src/main/java/com/example/organizze1/Activitys/LoginActivity.java
+++ app/src/main/java/com/example/organizze1/Activitys/LoginActivity.java
@@ -1,16 +1,114 @@
 package com.example.organizze1.Activitys;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.organizze1.Config.ConfiguracaoFirebase;
+import com.example.organizze1.Model.Usuario;
 import com.example.organizze1.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText senha;
+    private Button entrar;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.caixaEmail);
+        senha = findViewById(R.id.caixaSenha);
+        entrar = findViewById(R.id.buttonEntrar);
+
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String textoEmail = email.getText().toString();
+                String textoSenha = senha.getText().toString();
+
+
+
+                if(!textoEmail.isEmpty()){
+                    if(!textoSenha.isEmpty()){
+
+                        usuario = new Usuario();
+                        usuario.setEmail(textoEmail);
+                        usuario.setSenha(textoSenha);
+
+                        validarLogin();
+
+
+
+                        }else{
+                            Toast.makeText(LoginActivity.this,
+                                    "preencha o campo senha", Toast.LENGTH_LONG).show();
+                        }
+
+                    }else{
+                        Toast.makeText(LoginActivity.this,
+                                "Preencha o E-mail", Toast.LENGTH_LONG).show();
+                    }
+
+
+            }
+        });
+
+    }
+
+    public void validarLogin(){
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+
+                    Toast.makeText(LoginActivity.this, "autenticacao bem sucedida", Toast.LENGTH_LONG).show();
+
+                }else{
+
+                    String excecao = "";
+
+                    try{
+                        throw task.getException();
+
+                    }catch (FirebaseAuthInvalidUserException e){
+                        excecao = "usuário não cadastrado";
+                    }catch (FirebaseAuthInvalidCredentialsException e ){
+                        excecao = "Email ou senha não correspondem ao usuário cadastrado";
+                    }catch (Exception e ){
+                        excecao = "erro ao cadastrar usuário: " + e.getMessage();
+                        e.printStackTrace();
+                    }
+
+                    Toast.makeText(LoginActivity.this, excecao, Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
     }
 }
