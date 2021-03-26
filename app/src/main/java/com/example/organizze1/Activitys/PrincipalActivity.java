9b0e410f982f5b23d8f5e73e5e4e20e1f36afbb4
Alex Silva <damasioesilva@gmail.com>
2021-03-25 21:54:09 -0300

    add calendar view

--- app/src/main/java/com/example/organizze1/Activitys/PrincipalActivity.java
+++ app/src/main/java/com/example/organizze1/Activitys/PrincipalActivity.java
@@ -11,17 +11,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.organizze1.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    private MaterialCalendarView calendarView;
+    private TextView saudacao;
+    private TextView saldo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-        //Toolbar toolbar = findViewById(R.id.toolbar);
-       // setSupportActionBar(toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        calendarView = findViewById(R.id.calendarView);
+        saudacao = findViewById(R.id.textSaudacao);
+        saldo = findViewById(R.id.textSaldo);
+
+
+
+        configuraCalendarWiew();
+
+
 /*
         FloatingActionButton fab = findViewById(R.id.fab_label);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -44,5 +61,16 @@ public class PrincipalActivity extends AppCompatActivity {
 
         startActivity(new Intent(this, SaidaActivity.class));
 
+    }
+
+    public void configuraCalendarWiew(){
+        CharSequence meses[] = {"jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"};
+        calendarView.setTitleMonths(meses);
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+            }
+        });
     }
 }
